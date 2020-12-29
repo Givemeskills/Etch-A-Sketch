4203785d13fb78d032103b3dae55c647e7dc478d
Alex <alexddaintith94@gmail.com>
2020-12-29 17:21:34 +0000

    tidied up naming and added comments

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
 /* Essential variables
 Rows - a list of all rows
-column - a list of all columns
+cells - a list of all cells
+reset - reference to reset button
 */
-let table = document.querySelector("#gridContainer");
-let rows = document.getElementsByClassName("rowStyles");
-let column = document.getElementsByClassName("cellStyles");
-let reset = document.getElementById("reset");
+let table = document.getElementById("grid-container");
+let rows = document.getElementsByClassName("row-styles");
+let cells = document.getElementsByClassName("cell-styles");
+let reset = document.getElementById("reset-button");
 
 function makeRows(rowNum) {
 
     for (i = 0; i < rowNum; i++) {
         let row = document.createElement('div');
-        row.classList.add("rowStyles")
+        row.classList.add("row-styles")
         table.appendChild(row);
     }
 }
@@ -20,28 +21,30 @@ function makeColumns(columns) {
     for (i = 0; i < columns; i++) {
         for (j = 0; j < rows.length; j++){
             let cell = document.createElement('div');
-            cell.classList.add("cellStyles");
+            cell.classList.add("cell-styles");
             rows[j].appendChild(cell);
         }
     }
 }
 
-function createTable (r, c) {
-    makeRows(r);
-    makeColumns(c);
+function createTable (rowNum, columnNum) {
+    makeRows(rowNum);
+    makeColumns(columnNum);
 }
 
+// adds drawing functionality
 table.addEventListener("mouseover", function(event){
     event.target.style.backgroundColor = "black";
     console.log(event.target);
 })
 
+// 'erases' color by resetting background of all cells to white
 function resetColor() {
-    for(i=0; i < column.length; i++) {
-    column[i].style.backgroundColor = "white";
+    for(i=0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = "white";
 }}
 
 reset.addEventListener("click", resetColor);
 
-
+// starts page with 16x16 etch-a-sketch
 createTable(16,16);
