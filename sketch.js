2c35846581075276ad6613169e416fb1450b41e0
Alex <alexddaintith94@gmail.com>
2021-01-02 14:38:28 +0000

    added grid resizer (scales with container) with range slider and pen options

--- sketch.js
+++ sketch.js
@@ -7,6 +7,10 @@ let table = document.getElementById("grid-container");
 let rows = document.getElementsByClassName("row-styles");
 let cells = document.getElementsByClassName("cell-styles");
 let reset = document.getElementById("reset-button");
+let eraser = document.getElementById("eraser");
+let rainbowButton = document.getElementById("change-color-rainbow");
+let defaultButon = document.getElementById("change-color-default");
+let tableSize = 16;
 
 function makeRows(rowNum) {
 
@@ -14,6 +18,7 @@ function makeRows(rowNum) {
         let row = document.createElement('div');
         row.classList.add("row-styles")
         table.appendChild(row);
+        row.style.height = "calc(600px / " + tableSize + ")";
     }
 }
 
@@ -23,6 +28,7 @@ function makeColumns(columns) {
             let cell = document.createElement('div');
             cell.classList.add("cell-styles");
             rows[j].appendChild(cell);
+            cell.style.width = "calc(600px / " + tableSize +")";
         }
     }
 }
@@ -35,7 +41,6 @@ function createTable (rowNum, columnNum) {
 // adds drawing functionality
 table.addEventListener("mouseover", function(event){
     event.target.style.backgroundColor = "black";
-    console.log(event.target);
 })
 
 // 'erases' color by resetting background of all cells to white
@@ -44,7 +49,68 @@ function resetColor() {
     cells[i].style.backgroundColor = "white";
 }}
 
+// starts page with 16x16 etch-a-sketch
+createTable(tableSize, tableSize);
+resetColor(); // prevents all black glitch on load
+
+// reset button to clear sketchpad
 reset.addEventListener("click", resetColor);
 
-// starts page with 16x16 etch-a-sketch
-createTable(16,16);
+// 'eraser' button sets pen to white
+eraser.addEventListener("click", function(even) {
+    table.addEventListener("mouseover", function(event){
+        event.target.style.backgroundColor = "white";
+    })
+
+})
+
+// button to change color of pen to default (black)
+defaultButon.addEventListener("click", function(event){
+    
+    table.addEventListener("mouseover", function(event){
+        event.target.style.backgroundColor = "black";
+    })
+
+})
+
+// button to change color of pen to rainbow
+rainbowButton.addEventListener("click", function(event) {
+
+    table.addEventListener("mouseover", function(event){
+        generateColorValue();
+        event.target.style.backgroundColor = "rgb(" + colorValue[0] + "," + colorValue[1] + "," + colorValue[2] + ")";
+
+    })
+    
+}) 
+
+// Slider to alter size
+var rangeslider = document.getElementById("sliderRange");
+var output = document.getElementById("padSize");
+output.innerHTML = rangeslider.value;
+
+rangeslider.oninput = function() {
+  output.innerHTML = this.value;
+
+  for (i = 0; i < tableSize; i++) {
+    rows[0].remove();
+    
+    }
+    tableSize = rangeslider.value;
+    createTable(tableSize, tableSize);
+    resetColor();
+    }
+
+// generates 3 numbers between 0-255 which will be used as RGB values
+let colorValue = [];
+function generateColorValue() {
+    for (i = 0; i < 3; i++) {
+        colorValue[i] = (Math.floor((Math.random() * 255)));
+}
+    return colorValue;
+}
+
+
+
+
+
